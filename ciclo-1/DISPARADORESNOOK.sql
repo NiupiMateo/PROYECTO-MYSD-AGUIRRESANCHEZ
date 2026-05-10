55ac880fd2ffff9c298e2982d420ddcab36da8a8
Mateo Sanchez <mateosanchez022004@gmail.com>
2026-05-09 23:14:31 -0500

    Delete ciclo-1/DISPARADORESNOOK.sql

--- ciclo-1/DISPARADORESNOOK.sql
+++
@@ -1,9 +0,0 @@
-/* PROYECTO: Formando Campeones
-   CICLO 1
-   OBJETIVO: Pruebas incorrectas de disparadores */
-
-INSERT INTO Persona (idPersona, documento, nombres, apellidos, fechaNacimiento, telefono, correo)
-VALUES (20, '1000000020', 'Nino', 'Pequeno', DATE '2022-01-01', '3002020202', 'nino@mail.com');
-
-INSERT INTO Jugador (idPersona, posicion, numeroCamiseta)
-VALUES (20, 'DELANTERO', 11);
