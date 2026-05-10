81418f19f7fc585c6114516e1e3d2c7d78138204
Mateo Sanchez <mateosanchez022004@gmail.com>
2026-05-09 23:15:50 -0500

    Delete ciclo-1/TUPLASOK.sql

--- ciclo-1/TUPLASOK.sql
+++
@@ -1,8 +0,0 @@
-/* PROYECTO: Formando Campeones
-   CICLO 1
-   OBJETIVO: Ingreso correcto respecto a restricciones de tupla */
-
-INSERT INTO Participante (idPersona, idEntrenamiento, asistencia, rol, observaciones)
-VALUES (1, 1, 'S', 'JUGADOR', 'Asistio puntualmente');
-
-COMMIT;
